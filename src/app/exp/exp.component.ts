c07c49655a6c7ba45913ccf039f384ccf698a4ff
aolmo <aolmoher@uccs.edu>
2021-04-06 21:16:38 -0700

    Many new changes, check Github pages for details

--- src/app/exp/exp.component.ts
+++ src/app/exp/exp.component.ts
@@ -10,21 +10,21 @@ export class ExpComponent {
   exps = [
     {
       "title": "Research Assistant at ASU",
-      "dates": ["08/2018", "Present"],
+      "dates": ["Aug 2018", "May 2023"],
       "place": "Arizona State University",
-      "descr": "I am a Research Assistant in the Planning and Artificial Intelligence lab under the supervision of professor Subbarao Kambhampati at Arizona State University."
+      "descr": "I am a Research Assistant at the <a href='https://yochan-lab.github.io/home/'> Planning and Artificial Intelligence lab </a> under the supervision of professor Subbarao Kambhampati at Arizona State University."
     },
     {
       "title": "Research Assistant at UAB",
-      "dates": ["01/2018", "06/2018"],
+      "dates": ["Jan 2018", "Jun 2018"],
       "place": "Universitat Autónoma de Barcelona",
-      "descr": "I worked as a part-time Research Assistant developing the toolchain for the  <a href='https://github.com/HPCA4SE-UAB/MATE'> Monitoring Analysis and Tuning Environment</a> (MATE) software at Universitat Autònoma de Barcelona."
+      "descr": "I worked as a Research Assistant developing the toolchain for the  <a href='https://github.com/HPCA4SE-UAB/MATE'> Monitoring Analysis and Tuning Environment</a> (MATE) software at Universitat Autònoma de Barcelona."
     },
     {
       "title": "Front-End Developer at Delectatech",
-      "dates": ["01/2016", "06/2016"],
+      "dates": ["Jan 2016", "Jun 2016"],
       "place": "Delectatech",
-      "descr": "I was a front-end developer in AngularJS, Javascript, HTML5 and CSS3. In this company I also put Agile methodologies in practice."
+      "descr": "I was a front-end developer in AngularJS, Javascript, HTML5 and CSS3. Cide developed under Scrum's Agile principles."
     }
   ]
 }
--- src/app/honors/honors.component.ts
+++ src/app/honors/honors.component.ts
@@ -9,22 +9,22 @@ export class HonorsComponent {
 
   honors = [
     {
-      "title": "Balsells Fellowship",
-      "dates": ["08/2016", "12/2017"],
+      "title": "<a href='https://uccs.edu/balsellseng/'>Balsells Fellowship</a>",
+      "dates": ["Aug 2016", "Dec 2017"],
       "place": "University of Colorado at Colorado Springs",
-      "descr": "Scholarship that paid for my tuition and fees of my Computer Science Master's the University of Colorado at Colorado Springs as well as a stipend – only available for a very limited amount of students in Spain every year."
+      "descr": "Fellowship that covered in full the tuition of my Computer Science Master's degree at the University of Colorado at Colorado Springs as well as a stipend – only available for a very limited amount of students in Spain every year."
     },
     {
       "title": "Undergraduate Research Assistantship",
-      "dates": ["07/2015", "06/2016"],
+      "dates": ["Jul 2015", "Jun 2016"],
       "place": "Universitat Autònoma de Barcelona",
-      "descr": "Undergraduate grant awarded by the Computer Architecture and Operating Systems department at Universitat Autònoma de Barcelona that paid for my contribution to the research projects Periscope and ELASTIC."
+      "descr": "Undergraduate grant awarded by the Computer Architecture and Operating Systems department at Universitat Autònoma de Barcelona that paid for my contribution to the European research projects Periscope and ELASTIC."
     },
     {
       "title": "Baccalaureate with Honors",
-      "dates": ["06/2016", ""],
+      "dates": ["Jun 2016", ""],
       "place": "IES La Romànica",
-      "descr": "Received a grant that paid for all tuition and fees of the first year of my degree in Computer Science at Universitat Autònoma de Barcelona – Best student of 2012 in IES La Romànica"
+      "descr": "Undergraduate grant that paid for the first year of my degree in Computer Science at Universitat Autònoma de Barcelona – Best student of 2012 of IES La Romànica"
     },
   ]
 }
