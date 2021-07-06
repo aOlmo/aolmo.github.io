d3d75fef13ba83726a8c7f0295ab8c47fbb54bc8
aolmo <aolmoher@asu.edu>
2021-07-05 20:43:13 -0700

    Improve overall look of the site, change font name as well

--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -11,21 +11,22 @@ export class PublicationsComponent {
     {
       "Authors": "Alberto Olmo, Sarath Sreedharan, Subbarao Kambhampati",
       "Title": "GPT3-to-plan: Extracting plans from text using GPT-3",
-      "Publication": "ICAPS FinPlan and ICAPS Knowledge Engineering, 2021",
+      "Publication": "ICAPS FinPlan and ICAPS KEPS, 2021",
       "Download": "https://arxiv.org/abs/2106.07131"
     },
     {
       "Authors": "Niharika Jain, Alberto Olmo, Sailik Sengupta, Lydia Manikonda, Subbarao Kambhampati",
-      "Title": "Imperfect ImaGANation: Implications of GANs Exacerbating Biases on Facial Data Augmentation and Snapchat Selfie Lenses",
+      "Title": "Imperfect ImaGANation",
       "Publication": "ICLR SDG, 2021",
-      "Download": "https://arxiv.org/pdf/2001.09528"
+      "Download": "https://arxiv.org/pdf/2001.09528",
+      "Poster": "https://aolmo.github.io/files/imaganation_iclr_poster.pdf"
     },
     {
       "Authors": "Alberto Olmo, Sailik Sengupta, Subbarao Kambhampati",
       "Title": "Not all Failure Modes are Created Equal: Training Deep Neural Networks for Explicable (Mis) Classification",
-      "Publication": "ICML Workshop on Uncertainty and Robustness in Deep Learning (UDL) and Human-In-the-Loop Learning (HILL), 2020",
+      "Publication": "ICML UDL and ICML HILL, 2020",
       "Download": "https://arxiv.org/pdf/2006.14841",
-      "Poster": "https://aolmo.github.io/files/wlf_poster_icml.pdf"
+      "Poster": "https://aolmo.github.io/files/wlf_icml_poster.pdf"
     },
     {
       "Authors": "Sarath Sreedharan, Alberto Olmo, Aditya Prasad Mishra, Subbarao Kambhampati",
@@ -42,7 +43,7 @@ export class PublicationsComponent {
     {
       "Authors": "Zahra Zahedi, Alberto Olmo, Tathagata Chakraborti, Sarath Sreedharan, Subbarao Kambhampati",
       "Title": "Towards understanding user preferences for explanation types in model reconciliation",
-      "Publication": "International Conference on Human-Robot Interaction (HRI), 2019",
+      "Publication": "International Conference on Human-Robot Interaction, 2019",
       "Download": "https://ieeexplore.ieee.org/abstract/document/8673097"
     },
     {
@@ -53,7 +54,7 @@ export class PublicationsComponent {
     },
     {
       "Authors": "Alberto Olmo",
-      "Title": "Extending MPI Parameters' plugin",
+      "Title": "Extending MPI Parameters' Plugin",
       "Publication": "",
       "Download":"https://ddd.uab.cat/pub/tfg/2016/tfg_39376/Extending_PTFs_MPI_Parameters_plugin_-_Alberto_Olmo.pdf"
     }
